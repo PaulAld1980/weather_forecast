bf6f5bf2755f9853e7ad3a14e1e5969393d43f20
PaulAld1980 <hidden80@mail.ru>
2025-03-03 01:33:41 +0500

    2nd commit
    The code is fixed.

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import requests
 
 
-def get_forecast(locations):
-    for loc in locations:
-        url = f'https://wttr.in/{loc}?nTqm&lang=ru'
-        response = requests.get(url)
-        response.raise_for_status()
-        print(response.text)
+def get_forecast(location):
+    params = {
+        "n": "",
+        "T": "",
+        "q": "",
+        "m": "",
+        "lang": "ru",
+        "M": "",
+        "3": ""
+    }
+    url = f'https://wttr.in/{location}'
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.text
 
 
 def main():
     locations = ['Лондон', 'SVO', 'Череповец']
-    get_forecast(locations)
+    for place in locations:
+        weather = get_forecast(place)
+        print(weather)
 
 
 if __name__ == "__main__":
